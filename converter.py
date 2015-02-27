cfcf828b25dc15e4cfafa06504b99bcd45be27ff
seeker8 <chuyo_gk87@yahoo.com.mx>
2015-02-26 21:10:53 -0600

    Basic example of functions and dictionary

---
+++ converter.py
@@ -0,0 +1,19 @@
+def converterFromRomanToArabic(romanValue):
+    romans = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    res = 0
+    tempRes = 0
+    temp = 0
+    for value in romanValue:
+        decimal = romans[value]
+        if temp < decimal:
+            temp = temp * -1
+        tempRes = tempRes + temp
+        temp = decimal    
+    return tempRes + temp
+
+if __name__ == '__main__':
+    print converterFromRomanToArabic('XLIV')
+    print converterFromRomanToArabic('VIII')
+    print converterFromRomanToArabic('XX')
+    print converterFromRomanToArabic('MCMLXLIX')
+    print converterFromRomanToArabic('XIX')
